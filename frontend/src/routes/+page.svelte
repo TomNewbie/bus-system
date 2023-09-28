31303adba9fe25bab5dbe698f2f1abc510db3b30
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-09-28 15:13:54 +0700

    Push build files

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,18 @@
-<h1 class="text-red-200">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<body>
+	<div class="relative bg-slate-200 min-h-screen min-w-screen overflow-hidden">
+		<div class="absolute z-10 right-0 bottom-0 w-1/4 h-1/6 -translate-y-1/3 bg-white">
+			<div class="flex justify-between m-2">
+				<input class="border border-solid border-neutral-30 w-5/6" />
+				<p>Cancel</p>
+			</div>
+			<ul class="m-5 h-max">
+				<li>1</li>
+				<li>2</li>
+				<li>3</li>
+			</ul>
+		</div>
+	</div>
+</body>
 
 <style lang="postcss">
 </style>
