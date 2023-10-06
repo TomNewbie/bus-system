e99a73925e3e3adce2537c9d48eadf5138a337d8
MinhTran1506 <Your GitHub Email>
2023-10-06 13:22:17 +0700

    update data cleaning

--- Data/embedded/csv_to_mongo_doc_line.py
+++ Data/embedded/csv_to_mongo_doc_line.py
@@ -1,34 +1,40 @@
 import pandas as pd
 import json
 
-# Load the CSV files into pandas DataFrames
+# Define the chunk size
+chunk_size = 10000
+
+# Load the CSV files into DataFrames
 trips_df = pd.read_csv('trips.csv')
 stops_df = pd.read_csv('stops.csv')
 stop_times_df = pd.read_csv('stop_times.csv')
 
-# Inner join trips_df with stop_times_df on trip_id
-merged_df = trips_df.merge(stop_times_df, on='trip_id', how='inner')
-
-# Join with stops_df on stop_id
-merged_df = merged_df.merge(stops_df, on='stop_id', how='inner')
-
-filtered_routes_df = merged_df.dropna(subset=['start_stop_name', 'end_stop_name'])
-
-# Group by trip_id and aggregate the required information for each trip
+# Initialize an empty list to store the results
 routes_data = []
-for route_id, route_group in filtered_routes_df.groupby('route_id'):
-    route_attributes = route_group[['route_id', 'shape_id', 'start_stop_name', 'end_stop_name', 'agency_name']].iloc[0].to_dict()
-    
-    # Remove duplicate stops based on stop_id
-    unique_stops = route_group.drop_duplicates(subset='stop_id')[['stop_id', 'stop_sequence', 'stop_name', 'stop_lat', 'stop_lon']]
-    stops_info = unique_stops.to_dict(orient='records')
-    
-    route_data = {
-        'route_id': route_id,
-        **route_attributes,
-        'stops': stops_info
-    }
-    routes_data.append(route_data)
+
+# Process the CSV files in chunks
+for trips_chunk in pd.read_csv('RMV/Data/trips.csv', chunksize=chunk_size):
+    # Use the already loaded DataFrames in each iteration
+    merged_chunk = trips_chunk.merge(stop_times_df, on='trip_id', how='inner')
+    merged_chunk = merged_chunk.merge(stops_df, on='stop_id', how='inner')
+
+    # Filter routes with start_stop_name and end_stop_name not NaN
+    filtered_chunk = merged_chunk.dropna(subset=['start_stop_name', 'end_stop_name'])
+
+    # Group by route_id and aggregate the required information for each route in this chunk
+    for route_id, route_group in filtered_chunk.groupby('route_id'):
+        route_attributes = route_group[['route_id', 'shape_id', 'start_stop_name', 'end_stop_name', 'agency_name']].iloc[0].to_dict()
+        
+        # Remove duplicate stops based on stop_id
+        unique_stops = route_group.drop_duplicates(subset='stop_id')[['stop_id', 'stop_sequence', 'stop_name', 'stop_lat', 'stop_lon']]
+        stops_info = unique_stops.to_dict(orient='records')
+        
+        route_data = {
+            'route_id': route_id,
+            **route_attributes,
+            'stops': stops_info
+        }
+        routes_data.append(route_data)
 
 # Convert the result to a JSON object
 result_json = json.dumps(routes_data, indent=4)
--- Data/embedded/csv_to_mongo_doc_stop.py
+++ Data/embedded/csv_to_mongo_doc_stop.py
@@ -1,39 +1,42 @@
 import pandas as pd
 import json
 
+chunk_size = 10000
+
 # Load the CSV files into pandas DataFrames
 trips_df = pd.read_csv('trips.csv')
 stops_df = pd.read_csv('stops.csv')
 stop_times_df = pd.read_csv('stop_times.csv')
 
-# Inner join trips_df with stop_times_df on trip_id
-merged_df = trips_df.merge(stop_times_df, on='trip_id', how='inner')
-
-# Join with stops_df on stop_id
-merged_df = merged_df.merge(stops_df, on='stop_id', how='inner')
-
-# Filter trips with start_stop_name and end_stop_name not NaN
-filtered_trips_df = merged_df.dropna(subset=['start_stop_name', 'end_stop_name'])
-
-# Keep track of visited trip_ids to skip duplicates
-visited_trip_ids = set()
-
-# Group by stop_id and aggregate the required information for each stop
 stops_data = []
-for stop_id, stop_group in filtered_trips_df.groupby('stop_id'):
-    stop_attributes = stop_group[['stop_name', 'stop_id', 'stop_lat', 'stop_lon']].iloc[0].to_dict()
-    
-    # Extract trip information for this stop
-    trips_info = []
-    unique_routes = stop_group.drop_duplicates(subset='route_id')[['route_id','shape_id', 'start_stop_name', 'end_stop_name', 'agency_name']]
-    routes_info = unique_routes.to_dict(orient='records')
-    
-    stop_data = {
-        'stop_id': stop_id,
-        **stop_attributes,
-        'routes': routes_info
-    }
-    stops_data.append(stop_data)
+
+for trips_chunk in pd.read_csv('RMV/Data/trips.csv', chunksize=chunk_size):
+    # Inner join trips_df with stop_times_df on trip_id
+    merged_chunk = trips_chunk.merge(stop_times_df, on='trip_id', how='inner')
+
+    # Join with stops_df on stop_id
+    merged_chunk = merged_chunk.merge(stops_df, on='stop_id', how='inner')
+
+    # Filter trips with start_stop_name and end_stop_name not NaN
+    filered_chunk = merged_chunk.dropna(subset=['start_stop_name', 'end_stop_name'])
+
+    # Keep track of visited trip_ids to skip duplicates
+    visited_trip_ids = set()
+
+    for stop_id, stop_group in filered_chunk.groupby('stop_id'):
+        stop_attributes = stop_group[['stop_name', 'stop_id', 'stop_lat', 'stop_lon']].iloc[0].to_dict()
+        
+        # Extract trip information for this stop
+        trips_info = []
+        unique_routes = stop_group.drop_duplicates(subset='route_id')[['route_id','shape_id', 'start_stop_name', 'end_stop_name', 'agency_name']]
+        routes_info = unique_routes.to_dict(orient='records')
+        
+        stop_data = {
+            'stop_id': stop_id,
+            **stop_attributes,
+            'routes': routes_info
+        }
+        stops_data.append(stop_data)
 
 # Convert the result to a JSON object
 result_json = json.dumps(stops_data, indent=4)
