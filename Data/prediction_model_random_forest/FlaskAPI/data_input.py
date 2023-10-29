3428b119da68c78bcdc0b57a60907c9591399c2c
MinhTran1506 <117312295+MinhTran1506@users.noreply.github.com>
2023-10-29 21:47:33 +0700

    FlaskAPI update

--- Data/prediction_model_random_forest/FlaskAPI/data_input.py
+++ Data/prediction_model_random_forest/FlaskAPI/data_input.py
@@ -1,11 +1,36 @@
-data_in = [0.0,
- 12.0,
- 51.04977,
- 7.015134,
- 51.04977,
- 7.015134,
- 16.0,
- 24.2752987344503,
- 60.0,
- 1.0,
- 270.821859132056]
+data_in =[
+     {
+    "speed_kmh": 0.04390779,
+    "avg_route_speed_kmh": 0.09375,
+    "segment_max_speed_kmh": 0.08234218,
+    "runtime_sec": 0.05555556,
+    "start_stop_id": 0.11793839,
+    "end_stop_id": 0.11768656,
+    "distance_m": 0.01173629,
+    "stop_lat": 0.60638814,
+    "stop_lon": 0.41452295,
+    "next_lat": 0.6075418,
+    "next_lon": 0.41330337,
+    "arrival_hour": 0.34782609,
+    "arrival_minute": 0.15254237,
+    "route_id": 123,
+    "segment_id": 456
+},
+      {
+    "speed_kmh": 0.04390779,
+    "avg_route_speed_kmh": 0.09375,
+    "segment_max_speed_kmh": 0.08234218,
+    "runtime_sec": 0.05555556,
+    "start_stop_id": 0.11793839,
+    "end_stop_id": 0.11768656,
+    "distance_m": 0.01173629,
+    "stop_lat": 0.60638814,
+    "stop_lon": 0.41452295,
+    "next_lat": 0.6075418,
+    "next_lon": 0.41330337,
+    "arrival_hour": 0.34782609,
+    "arrival_minute": 0.15254237,
+    "route_id": 123,
+    "segment_id": 456
+}
+]
