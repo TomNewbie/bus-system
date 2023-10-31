6c31085accb71eca7b48fce61aba2da336ae3b83
Nguyễn Ngọc Vĩnh <122826039+vinhDev3006@users.noreply.github.com>
2023-10-31 23:22:25 +0700

    create prediction_model_lstm in backend folder

---
+++ backend/prediction_model_lstm/request.py
@@ -0,0 +1,17 @@
+import requests
+
+# Set the URL of your Flask server
+url = "http://127.0.0.1:5000/predict_congestion_lstm"
+
+# Define the data to send in the request
+data = {
+    "route_id": 1700960,
+    "direction_id": 1,
+    "future_time": "2023-10-25 23:50:00"
+}
+
+# Send the POST request
+response = requests.post(url, json=data)
+
+# Extract and print the JSON response
+print(response.json())
