c32dbcdaaeeb77d5f687127a3a2cd0d7ad476219
NguyenLe1605 <84883483+NguyenLe1605@users.noreply.github.com>
2023-11-01 17:56:06 +0700

    Fetching data for prediction request

--- backend/src/api/predict.rs
+++ backend/src/api/predict.rs
@@ -1,11 +1,18 @@
 use actix_web::{
-    post,
-    web::{self, ServiceConfig},
+    get, post,
+    web::{self, Data, Query, ServiceConfig},
     HttpResponse,
 };
-use bson::Document;
+use bson::{doc, Bson, Document};
+use chrono::{Duration, Timelike, Utc};
+use futures::TryStreamExt;
 use lazy_static::lazy_static;
+use mongodb::{error::Error, Client, Collection};
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use tokio::try_join;
+
+use crate::models::{route_info::RouteInfo, Segment};
 
 lazy_static! {
     static ref URL: &'static str = std::env::var("APP_ENVIRONMENT")
@@ -14,7 +21,135 @@ lazy_static! {
 }
 
 pub fn predict_config(cfg: &mut ServiceConfig) {
-    cfg.service(web::scope("/predict").service(dummy_predict));
+    cfg.service(
+        web::scope("/predict")
+            .service(dummy_predict)
+            .service(predict_congestion),
+    );
+}
+
+#[derive(Debug, Deserialize)]
+pub struct SegmentInfo {
+    route_id: String,
+    shape_id: String,
+    direction_id: i64,
+}
+
+async fn fetch_segments(
+    db_client: &Data<Client>,
+    shape_id: &String,
+    route_id: &String,
+    direction_id: i64,
+) -> Result<Vec<Segment>, Error> {
+    let col: Collection<Document> = db_client.database("bus").collection("segments");
+    let filter = doc! {
+        "properties.shape_id": shape_id,
+        "properties.route_id": route_id,
+        "properties.direction_id": Bson::Int64(direction_id),
+    };
+    let mut cursor = col.find(filter, None).await?;
+    let mut segments: Vec<Segment> = Vec::new();
+    while let Some(doc) = cursor.try_next().await? {
+        let segment = bson::from_document(doc)?;
+        segments.push(segment);
+    }
+    segments.sort_unstable_by_key(|k| k.properties.stop_sequence);
+    Ok(segments)
+}
+
+async fn fetch_route_info(
+    db_client: &Data<Client>,
+    route_id: i64,
+    direction_id: i64,
+) -> Result<Vec<RouteInfo>, Error> {
+    let col: Collection<Document> = db_client.database("bus").collection("route_info");
+    let filter = doc! {
+        "route_id": Bson::Int64(route_id),
+        "direction_id": Bson::Int64(direction_id),
+    };
+    let mut cursor = col.find(filter, None).await?;
+    let mut routes: Vec<RouteInfo> = Vec::new();
+    while let Some(doc) = cursor.try_next().await? {
+        let route = bson::from_document(doc)?;
+        routes.push(route);
+    }
+    routes.sort_unstable_by_key(|k| k.stop_sequence_x);
+    Ok(routes)
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct PredictionSegment {
+    segment_id: String,
+    start_stop_id: i64,
+    stop_lat: f64,
+    stop_lon: f64,
+    end_stop_id: i64,
+    next_lat: f64,
+    next_lon: f64,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct PredictionRoute {
+    route_id: i64,
+    direction_id: i64,
+    arrival_hour: u32,
+    arrival_minute: u32,
+    segments: Vec<PredictionSegment>,
+}
+
+#[get("")]
+pub async fn predict_congestion(
+    db_client: Data<Client>,
+    segment_info: Query<SegmentInfo>,
+) -> HttpResponse {
+    let time_for_prediction = (Utc::now() + Duration::minutes(10)).time();
+    let SegmentInfo {
+        route_id,
+        shape_id,
+        direction_id,
+    } = segment_info.into_inner();
+    tracing::info!("Request Info: Id of route - {route_id}, Id of shape - {shape_id}");
+
+    let route_id_i64 = match route_id.parse::<i64>() {
+        Ok(route_id) => route_id,
+        Err(err) => {
+            tracing::error!("Failed to execute parsing number: {}", err);
+            return HttpResponse::BadRequest().finish();
+        }
+    };
+    let routes = fetch_route_info(&db_client, route_id_i64, direction_id);
+    let segments = fetch_segments(&db_client, &shape_id, &route_id, direction_id);
+    let joined_stream = try_join!(routes, segments);
+    let (routes, segments) = match joined_stream {
+        Ok(joined_output) => joined_output,
+        Err(err) => {
+            tracing::error!("Failed to execute query: {:?}", err);
+            return HttpResponse::InternalServerError().finish();
+        }
+    };
+    tracing::info!("Len of segments: {}", segments.len());
+    tracing::info!("Len of routes: {}", routes.len());
+    let prediction_segments = routes
+        .iter()
+        .zip(segments.iter())
+        .map(|(route, segment)| PredictionSegment {
+            segment_id: segment.properties.segment_id.clone(),
+            start_stop_id: route.start_stop_id,
+            stop_lat: route.stop_lat,
+            stop_lon: route.stop_lon,
+            end_stop_id: route.start_stop_id,
+            next_lat: route.next_lat,
+            next_lon: route.next_lon,
+        })
+        .collect::<Vec<_>>();
+    let prediction_route = PredictionRoute {
+        segments: prediction_segments,
+        route_id: route_id_i64,
+        direction_id,
+        arrival_hour: time_for_prediction.hour(),
+        arrival_minute: time_for_prediction.minute(),
+    };
+    HttpResponse::Ok().json(prediction_route)
 }
 
 // Dummy route to test for connection to tensorflow serving for model deployment
@@ -45,7 +180,7 @@ pub async fn dummy_predict() -> HttpResponse {
         }
     };
 
-    let status = resp.status(); 
+    let status = resp.status();
     let resp: Document = match resp.json().await {
         Ok(resp) => resp,
         Err(err) => {
---
+++ backend/src/models/segment.rs
@@ -0,0 +1,34 @@
+use bson::oid::ObjectId;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Segment {
+    pub _id: ObjectId,
+    #[serde(rename = "type")]
+    pub _type: String,
+    pub properties: Properties,
+    pub geometry: Geometry,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Properties {
+    pub shape_id: String,
+    pub route_id: String,
+    pub route_name: String,
+    pub direction_id: i64,
+    pub stop_sequence: i64,
+    pub segment_name: String,
+    pub start_stop_name: String,
+    pub end_stop_name: String,
+    pub segment_id: String,
+    pub start_stop_id: String,
+    pub end_stop_id: String,
+    pub distance_m: f64,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Geometry {
+    #[serde(rename = "type")]
+    pub _type: String,
+    pub coordinates: Vec<[f64; 2]>,
+}
