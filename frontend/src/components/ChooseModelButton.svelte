e49d92c2c31b835077da173993aa3151d461c88f
Tho Phan <phanchitho18902@gmail.com>
2023-11-15 21:13:25 +0700

    integrate random forest model

--- frontend/src/components/ChooseModelButton.svelte
+++ frontend/src/components/ChooseModelButton.svelte
@@ -1,32 +1,46 @@
 <script>
 	// @ts-nocheck
-	import { currentIndex, minute } from '../stores/stores';
+	import { currentIndex, minute, model } from '../stores/stores';
+	import { lstmModel, randomForestModel } from '../utils/constants';
 
 	let busLinePopoverVisible;
 	$: {
 		setupButtonState($currentIndex);
 	}
 
+	function toggleButton(buttonId) {
+		model.set(buttonId);
+	}
+
 	function setupButtonState(currentIndex) {
 		if (currentIndex != -1) {
 			busLinePopoverVisible = true;
 		} else {
-			setDefaultState();
+			busLinePopoverVisible = false;
 		}
-	}
-
-	function setDefaultState() {
-		busLinePopoverVisible = false;
 	}
 </script>
 
 <body class="relative flex justify-end mt-20 mr-3" class:hidden={!busLinePopoverVisible}>
 	<div class="absolute flex justify-center gap-4">
-		<button class="px-3 py-2 bg-white rounded-lg active:bg-[#43D224] hover:bg-[#43D224]"
+		<button
+			on:click={() => toggleButton(lstmModel)}
+			class:activeButton={$model === lstmModel}
+			class="px-3 py-2 bg-white rounded-lg active:bg-[#43D224] hover:bg-[#43D224] focus:outline-none]"
 			>LSTM</button
 		>
-		<button class="px-3 py-2 bg-white rounded-lg active:bg-[rgb(67,210,36)] hover:bg-[#43D224]">
+		<button
+			on:click={() => toggleButton(randomForestModel)}
+			class:activeButton={$model === randomForestModel}
+			class="px-3 py-2 bg-white rounded-lg active:bg-[#43D224] hover:bg-[#43D224] focus:outline-none"
+		>
 			Random Forest
 		</button>
 	</div>
 </body>
+
+<style>
+	.activeButton {
+		background-color: #43d224;
+	}
+</style>
