cd7ae7ba4a032160fb03fefc71948c9eddcb1017
Tho Phan <phanchitho18902@gmail.com>
2023-11-16 01:03:03 +0700

    migrate to ts

---
+++ frontend/src/controller/congestion.ts
@@ -0,0 +1,107 @@
+import type { Map } from 'mapbox-gl';
+import { setDisableLayer } from './visualization';
+import { fetchCongestionDataByBusline } from '../services/map';
+
+let abortController: AbortController;
+let previousIndex = -1;
+const congestionColors: {
+	[key: number]: string;
+} = {
+	0: '#43D224', // Congestion level 1
+	1: '#FFE58F', // Congestion level 2
+	2: '#FE6240', // Congestion level 3
+	3: '#fc7a7a', // Congestion level 4
+	4: '#B60606' // Congestion level 5
+};
+
+function drawCongestionLevel(
+	busNetwork: BusNetwork,
+	currentBusLine: BusLineWithCongestionLevel,
+	map: Map
+) {
+	if (currentBusLine == undefined) return;
+	setDisableLayer(busNetwork, -1, map);
+
+	currentBusLine.forEach((segment, segmentIndex) => {
+		// Access the congestion level of the segment
+		const congestionLevel = segment.congestion_level;
+
+		// Determine the color based on congestion level
+		const color = congestionColors[congestionLevel];
+
+		// Create a source and layer for each segment
+		map.addSource(`segment_${segmentIndex}`, {
+			type: 'geojson',
+			data: {
+				type: 'FeatureCollection',
+				//@ts-ignore
+				features: [segment]
+			}
+		});
+
+		map.addLayer({
+			id: `segment_${segmentIndex}`,
+			type: 'line',
+			source: `segment_${segmentIndex}`,
+			layout: {
+				'line-join': 'miter',
+				'line-cap': 'round'
+			},
+			paint: {
+				'line-color': color,
+				'line-width': 4
+			}
+		});
+	});
+}
+
+export async function fetchCongestionData(
+	busNetwork: BusNetwork,
+	currentIndex: number,
+	minute: number,
+	map: Map
+) {
+	deleteCongestionLevel(busNetwork, map);
+	if (minute == 0) return;
+	if (currentIndex == -1 || currentIndex == undefined) return;
+	previousIndex = currentIndex;
+	let properties = busNetwork[currentIndex][0].properties;
+	// fetch data
+	abortController = new AbortController();
+
+	let result = await fetchCongestionDataByBusline(
+		'lstm',
+		properties.route_id,
+		properties.shape_id,
+		properties.direction_id,
+		minute,
+		abortController
+	);
+
+	drawCongestionLevel(busNetwork, result, map);
+}
+
+export function deleteCongestionLevel(busNetwork: BusNetwork, map: Map) {
+	if (previousIndex == -1 || previousIndex == undefined) return;
+	console.log('hehehe');
+	console.log(previousIndex);
+	if (abortController) {
+		abortController.abort();
+	}
+
+	if (!map.getLayer(`segment_0`)) {
+		return;
+	}
+
+	let currentBusLine = busNetwork[previousIndex];
+	currentBusLine.forEach((_, segmentIndex) => {
+		const layerId = `segment_${segmentIndex}`;
+		if (map.getLayer(layerId)) {
+			map.removeLayer(layerId);
+		}
+
+		if (map.getSource(layerId)) {
+			map.removeSource(layerId);
+		}
+	});
+}
---
+++ frontend/src/services/map.ts
@@ -0,0 +1,64 @@
+export async function fetchBusLine() {
+	let a = [];
+	try {
+		const response = await fetch('http://localhost:8000/segments');
+		if (response.ok) {
+			const data: GeoJsonWithId[] = await response.json();
+			console.log(data);
+			// Process the data and remove the "_id" attribute
+			a = data.map((item: any) => {
+				const { _id, ...itemWithoutId } = item;
+				return itemWithoutId;
+			});
+		} else {
+			alert('Request failed with status: ' + response.status);
+		}
+	} catch (error) {
+		alert('Error while fetching data: ' + error);
+	}
+	const groupedData: GeoJsonGroupByShapeId = {};
+	a.forEach((feature) => {
+		const shapeId = feature.properties.shape_id;
+		if (!groupedData[shapeId]) {
+			groupedData[shapeId] = [];
+		}
+		groupedData[shapeId].push(feature);
+	});
+	return Object.values(groupedData);
+}
+
+export async function fetchCongestionDataByBusline(
+	model: string,
+	route_id: string,
+	shape_id: string,
+	direction_id: number,
+	minute: number,
+	abortController: AbortController
+) {
+	if (minute == 0) return;
+	// fetch data
+	let result;
+	let abortSignal = abortController.signal;
+	try {
+		const response = await fetch(
+			`http://localhost:8000/predict/${model}?route_id=${route_id}&shape_id=${shape_id}&direction_id=${direction_id}&minute_predict=${minute}`,
+			{ signal: abortSignal }
+		);
+		if (response.ok) {
+			const data = await response.json();
+			// Process the data and remove the "_id" attribute
+			//@ts-ignore
+			result = data.map((item) => {
+				const { _id, ...itemWithoutId } = item;
+				return itemWithoutId;
+			});
+		} else {
+			alert('Request failed with status: ' + response.status);
+			return;
+		}
+	} catch (error) {
+		alert('Error while fetching data: ' + error);
+		return;
+	}
+	return result;
+}
