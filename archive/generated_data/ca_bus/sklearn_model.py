aeb8d22edca8aaf7c2a5df334dfda97fd3be4049
Nguyễn Ngọc Vĩnh <122826039+vinhDev3006@users.noreply.github.com>
2023-12-03 14:48:21 +0700

    Update with increamental learning

--- archive/generated_data/ca_bus/sklearn_model.py
+++ archive/generated_data/ca_bus/sklearn_model.py
@@ -29,7 +29,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.6, random_
 # Create models
 # clf = DecisionTreeClassifier()
 # clf = RandomForestClassifier(n_estimators=100, random_state=42)
-clf = SVC(kernel='rbf', random_state=42)
+clf = SVC(kernel='rbf', C=1.0, gamma='scale', random_state=42)
 # clf = KNeighborsClassifier(n_neighbors=5)
 
 
@@ -42,3 +42,28 @@ y_pred = clf.predict(X_test)
 # Evaluate the model
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy}")
+
+
+# ------------------------------------------------- INCREMENTAL LEARNING --------------------------------------------------------- #
+
+
+# Simulate new incoming data
+df_new_data = pd.read_csv('archive/generated_data/ca_bus/merged_data_5.csv')
+
+# Update the label encoder with new data
+df_new_data['route_id'] = le_route.transform(df_new_data['route_id'])
+df_new_data['stop_id'] = le_stop.transform(df_new_data['stop_id'])
+
+# Select features and target variable for new data
+X_new_data = df_new_data[['route_id', 'stop_id', 'latitude', 'longitude', 'speed']]
+y_new_data = df_new_data['congestion_level']
+
+# Update the model with new data
+clf.fit(X_new_data, y_new_data)
+
+# Make predictions on the new test set
+y_pred_new_data = clf.predict(X_new_data)
+
+# Evaluate the updated model
+accuracy_new_data = accuracy_score(y_new_data, y_pred_new_data)
+print(f"Updated Model Accuracy: {accuracy_new_data}")
