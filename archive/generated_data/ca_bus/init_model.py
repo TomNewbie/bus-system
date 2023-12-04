3cdbc530e8543fb817dc73f8bdc15af3fa71fe52
Nguyễn Ngọc Vĩnh <122826039+vinhDev3006@users.noreply.github.com>
2023-12-04 16:57:01 +0700

    update

--- archive/generated_data/ca_bus/init_model.py
+++ archive/generated_data/ca_bus/init_model.py
@@ -1,28 +1,55 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-# Models
+# Models Classifiers
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 
+
+# Models Regressors
+# from sklearn.svm import SVR 
+  
+  
+  
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 
 from joblib import dump, load
 
 # Read the dataset
-df = pd.read_csv('archive/generated_data/ca_bus/merged_data_5.csv')
+df = pd.read_csv('archive/generated_data/ca_bus/part_1.csv')
+
 
 # Label encoder for categorical variables
-le_route = LabelEncoder()
-le_stop = LabelEncoder()
-df['route_id'] = le_route.fit_transform(df['route_id'])
-df['stop_id'] = le_stop.fit_transform(df['stop_id'])
+le__id = LabelEncoder()
+le_id = LabelEncoder()
+le_trip_id = LabelEncoder()
+le_schedule_relationship = LabelEncoder()
+le_route_id = LabelEncoder()
+le_direction_id = LabelEncoder()
+le_current_status = LabelEncoder()
+le_time_stamp = LabelEncoder()
+le_stop_sequence = LabelEncoder()
+
+df['_id'] = le__id.fit_transform(df['_id'])
+df['id'] = le_id.fit_transform(df['id'])
+df['trip_id'] = le_trip_id.fit_transform(df['trip_id'])
+df['schedule_relationship'] = le_schedule_relationship.fit_transform(df['schedule_relationship'])
+df['route_id'] = le_route_id.fit_transform(df['route_id'])
+df['direction_id'] = le_direction_id.fit_transform(df['direction_id'])
+df['current_status'] = le_current_status.fit_transform(df['current_status'])
+df['timestamp'] = le_time_stamp.fit_transform(df['timestamp'])
+df['current_stop_sequence'] = le_stop_sequence.fit_transform(df['current_stop_sequence'])
 
 # Select features and target variable
-X = df[['route_id', 'stop_id', 'latitude', 'longitude', 'speed']]
+X = df[['_id', 'id', 'trip_id', 'schedule_relationship', 'route_id',
+       'direction_id', 'latitude', 'longitude', 'odometer', 'speed',
+       'current_stop_sequence', 'current_status', 'timestamp', 'stop_id'
+       ]]
+
+
 y = df['congestion_level']
 
 # Split the data into training and testing sets
@@ -34,6 +61,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.6, random_
 clf = SVC(kernel='rbf', C=1.0, gamma='scale', random_state=42)
 # clf = KNeighborsClassifier(n_neighbors=5)
 
+# clf = SVR(kernel='rbf') 
 
 # Train the model
 clf.fit(X_train, y_train)
--- archive/generated_data/ca_bus/update_model.py
+++ archive/generated_data/ca_bus/update_model.py
@@ -7,18 +7,40 @@ from sklearn.preprocessing import LabelEncoder
 from joblib import dump, load
 
 # Simulate new incoming data
-df_new_data = pd.read_csv('archive/generated_data/ca_bus/merged_data_5.csv')
+df_new_data = pd.read_csv('archive/generated_data/ca_bus/part_4.csv')
 
 clf = load('archive\generated_data\ca_bus\model.joblib')
 
 # Label encoder for categorical variables
-le_route = LabelEncoder()
-le_stop = LabelEncoder()
-df_new_data['route_id'] = le_route.fit_transform(df_new_data['route_id'])
-df_new_data['stop_id'] = le_stop.fit_transform(df_new_data['stop_id'])
+# Label encoder for categorical variables
+le__id = LabelEncoder()
+le_id = LabelEncoder()
+le_trip_id = LabelEncoder()
+le_schedule_relationship = LabelEncoder()
+le_route_id = LabelEncoder()
+le_direction_id = LabelEncoder()
+le_current_status = LabelEncoder()
+le_time_stamp = LabelEncoder()
+le_stop_sequence = LabelEncoder()
 
-# Select features and target variable for new data
-X_new_data = df_new_data[['route_id', 'stop_id', 'latitude', 'longitude', 'speed']]
+
+df_new_data['_id'] = le__id.fit_transform(df_new_data['_id'])
+df_new_data['id'] = le_id.fit_transform(df_new_data['id'])
+df_new_data['trip_id'] = le_trip_id.fit_transform(df_new_data['trip_id'])
+df_new_data['schedule_relationship'] = le_schedule_relationship.fit_transform(df_new_data['schedule_relationship'])
+df_new_data['route_id'] = le_route_id.fit_transform(df_new_data['route_id'])
+df_new_data['direction_id'] = le_direction_id.fit_transform(df_new_data['direction_id'])
+df_new_data['current_status'] = le_current_status.fit_transform(df_new_data['current_status'])
+df_new_data['timestamp'] = le_time_stamp.fit_transform(df_new_data['timestamp'])
+df_new_data['current_stop_sequence'] = le_stop_sequence.fit_transform(df_new_data['current_stop_sequence'])
+
+# Select features and target variable
+X_new_data = df_new_data[['_id', 'id', 'trip_id', 'schedule_relationship', 'route_id',
+       'direction_id', 'latitude', 'longitude', 'odometer', 'speed',
+       'current_stop_sequence', 'current_status', 'timestamp', 'stop_id'
+       ]]
+
+
 y_new_data = df_new_data['congestion_level']
 
 # Update the model with new data
