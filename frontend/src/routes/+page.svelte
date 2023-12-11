3220800ceb85ec1e73cc44b21225d85ab9399796
Le Hoang Kim Thanh <81308733+Kimthanh11@users.noreply.github.com>
2023-12-11 14:37:16 +0700

    Update route for home

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,19 +4,13 @@
 	import MapCa from '../components/MapCA.svelte';
 	import MapDe from '../components/MapDE.svelte';
 
-	export let baseUrl = 'http://127.0.0.1:5173/';
-	export let path = '';
-
-	let url = baseUrl + path; // Updated URL construction
-
-	// Make sure to handle changes in the path variable
-	$: url = baseUrl + path;
+	export let url = '';
 </script>
 
 <Router {url}>
 	<div>
+		<Route path="/"><Home /></Route>
 		<Route path="/de" component={MapDe} />
 		<Route path="/ca" component={MapCa} />
-		<Route path="" component={Home} />
 	</div>
 </Router>
